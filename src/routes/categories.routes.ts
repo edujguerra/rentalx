b83079df6d7fe51f6c0c82c5fbee00ce05990271
Eduardo J. Guerra <edujguerra@gmail.com>
2022-12-30 22:44:44 -0300

    2212302244

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,25 +1,27 @@
 import { Router } from "express";
-import { v4 as uuidv4 } from "uuid";
-import { Category } from "../model/Category";
+import { CategoriesRepository } from "../repositories/CategoriesRepository";
 
 const categoriesRoutes = Router();
+const categoriesRepository = new CategoriesRepository();
 
-const categories: Category[] = [];
 
 categoriesRoutes.post("/", (request,response) =>{
     const { name, description } = request.body;
-
-    const category = new Category()
-    Object.assign(category,{
-        name,
-        description,
-        created_at: new Date()
-    })
     
+    const categoryAlreadyExists = categoriesRepository.findByName(name);
+    if (categoryAlreadyExists) {
+        return response.status(400).json({error: "Category already exists..."})
+    }
 
-    categories.push(category);
-
-    return response.status(201).json({category});
+    categoriesRepository.create({name,description})
+    return response.status(201).send();
 })
+
+
+categoriesRoutes.get("/", (request,response) =>{
+    const all =  categoriesRepository.list();
+    return response.json(all);
+})
+
 
 export  {categoriesRoutes};
